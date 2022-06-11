cb11c09d4f56e7e7d61c7606665713e7dd6f6031
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2022-06-11 14:52:54 +0000

    Frontend/Add/formUI/2

--- frontend/src/components/RegisterPin/registerpin.component.jsx
+++ frontend/src/components/RegisterPin/registerpin.component.jsx
@@ -1,25 +1,141 @@
 import React from "react";
 import styles from "./registerpin.module.css";
-
-import { Formik, Field, Form } from 'formik';
-
+import * as Yup from "yup";
+import { Formik, Field, Form } from "formik";
+import InputForm from "../FormElements/Input/input.component";
+import TextField from "@mui/material/TextField";
 const RegisterPin = () => {
+  const handleSubmit = (values) => {
+    console.log("values", values);
+  };
   return (
     <>
       {/* small heading */}
       <div className={styles.navbar}>
-        <h1>j
+        <h2>
           Choose location from the map and Register your public place here
-        </h1>
+        </h2>
       </div>
       {/* form to register  */}
       <div className={styles.formContainer}>
-        <form className={styles.form}>
+        <Formik
+          initialValues={{
+            username: "",
+            title: "",
+            desc: "",
+          }}
+          onSubmit={handleSubmit}
+        >
+          <Form>
+            <Field
+              as={TextField}
+              variant="outlined"
+              helperText="make sure to enter complete name"
+              label="Name of the owner"
+              id="username"
+              name="username"
+              placeholder="Mr.John Doe"
+            />
 
-        </form>
+            <Field    as={TextField}
+              variant="outlined"
+              helperText="don't include address in title"
+              label="Name of the Shop/School/Place" id="title" name="title" placeholder="Ex. City hospital" />
+
+            <Field
+            as={TextField}
+            variant="outlined"
+            helperText="enter a brief description"
+            label="Description: "
+              id="desc"
+              name="desc"
+              placeholder="Ex.We provide various sevices for disabled people like ...."
+              type="textarea"
+            />
+            <button type="submit">Submit</button>
+          </Form>
+        </Formik>
       </div>
     </>
   );
 };
 
 export default RegisterPin;
+
+// const validationSchema = Yup.object({
+//   submitstudentName: Yup.string().required("name needed"),
+//   fatherName: Yup.string().required("fathername needed"),
+//   address: Yup.string()
+//     .min(20, "Must be 20 character or less")
+//     .required("address is required"),
+//   phone: Yup.string()
+//     .min(10, "Must be 10 characters")
+//     .required("phone is required"),
+//   selectStandard: Yup.string().required("please select standard"),
+//   dob: Yup.date("please give correct date"),
+//   refCode: Yup.string().required("Refercode is required"),
+// });
+
+// <Formik
+// className={styles.form}
+// initialValues={{
+//   username: "",
+//   title: "",
+// }}
+// validationSchema={validationSchema}
+// onSubmit={handleSubmit}
+// >
+// {(formik) => {
+//   const { errors, touched } = formik;
+
+//   return (
+//     <Form>
+//       <InputForm
+//         control="input"
+//         type="text"
+//         label="Name of the Owner"
+//         isTouched={touched.username}
+//         fullWidth="true"
+//         name="username"
+//         errMsg={errors.username}
+//         placeholder="Ex.Mr. John doe"
+//         className={styles.inputsIn}
+//       />
+//       <InputForm
+//         control="input"
+//         type="text"
+//         label="Name of the Shop"
+//         isTouched={touched.title}
+//         fullWidth="true"
+//         name="title"
+//         errMsg={errors.title}
+//         placeholder="Ex.Mr. John doe"
+//         className={styles.inputsIn}
+//       />
+//       <InputForm
+//         control="input"
+//         type="text"
+//         label="Name of the Shop "
+//         isTouched={touched.firstName}
+//         fullWidth="true"
+//         name="firstName"
+//         errMsg={errors.FirstName}
+//         placeholder="Ex. City Hospital"
+//         className={styles.inputsIn}
+//       />
+//       <InputForm
+//         control="input"
+//         type="text"
+//         label="Description of the shop"
+//         isTouched={touched.desc}
+//         fullWidth="true"
+//         name="desc"
+//         errMsg={errors.desc}
+//         placeholder="Ex.We provide various support for disabled peoples."
+//         className={styles.inputsIn}
+//       />
+//       <button type="submit">Submit</button>
+//     </Form>
+//   );
+// }}
+// </Formik>
