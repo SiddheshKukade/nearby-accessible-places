48171596a801a3fe2bbf6558d2724e0f97741da3
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2022-06-11 07:20:13 +0000

    backend/add/schema

---
+++ backend/models/Pin.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+// Pin is a landmarks that people will register it as accessible
+const PinSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 60,
+    },
+    desc: {
+      type: String,
+      required: true,
+      min: 3,
+    },
+    long: {
+      type: Number,
+      required: true,
+    },
+    lat: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true } // to get created at and updated at timestams automatically 
+);
+
+module.exports = mongoose.model("Pin", PinSchema);
