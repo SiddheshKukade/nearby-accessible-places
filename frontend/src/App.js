6002470452f3343febe032437cee3003b60bc3b5
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2022-06-12 12:30:51 +0000

    Frontend/Add/MUI/theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,56 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext, createContext } from "react";
 import MapComponent from "./components/Map/map.component";
 import styles from "./App.module.css"; // Import css modules stylesheet as styles
 import RegisterPin from "./components/RegisterPin/registerpin.component";
 import AdjustIcon from "@mui/icons-material/Adjust";
 import { Tooltip } from "@mui/material";
+
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+export const MapContext = createContext({ lat: 0, lng: 0 });
+export const PinListContext = createContext([]);
+
 function App() {
   const [render, setRender] = useState(false);
+  const [selectedLatLng, setSelectedLatLng] = useState({ lat: 0, lng: 0 });
+  const [pinList, setPinList] = useState([]);
   const handleRender = () => {
     setRender((prev) => !prev);
   };
+  const THEME = createTheme({
+    typography: {
+      fontFamily: `"Poppins", "Helvetica", "Arial", sans-serif`,
+      fontSize: 14,
+      fontWeightLight: 300,
+      fontWeightRegular: 400,
+      fontWeightMedium: 500,
+    },
+  });
   return (
-    <div className={styles.App}>
-      <div className={styles.navbar}>
-      ForAll - see your nearby supported accessible public places.{" "}
-      </div>
-        <Tooltip title="Click the button to Re-center the map">
-      <div className={styles.centerButton} onClick={handleRender}>
-          <AdjustIcon />
-      </div>
-        </Tooltip>
-      {/* -- register your store and category -- show it on map */}
-      <div className={styles.mapContainer}>
-        <MapComponent />
-      </div>
-      <div className={styles.register}>
-        {" "}
-        <RegisterPin />
-      </div>
-    </div>
+    <ThemeProvider theme={THEME}>
+      <PinListContext.Provider value={{ pinList, setPinList }}>
+        <MapContext.Provider value={{ selectedLatLng, setSelectedLatLng }}>
+          <div className={styles.App}>
+            <div className={styles.navbar}>
+              ForAll - see your nearby supported accessible public places.{" "}
+            </div>
+            <Tooltip title="Click the button to Re-center the map">
+              <div className={styles.centerButton} onClick={handleRender}>
+                <AdjustIcon />
+              </div>
+            </Tooltip>
+            {/* -- register your store and category -- show it on map */}
+            <div className={styles.mapContainer}>
+              <MapComponent />
+            </div>
+            <div className={styles.register}>
+              {" "}
+              <RegisterPin />
+            </div>
+          </div>
+        </MapContext.Provider>
+      </PinListContext.Provider>
+    </ThemeProvider>
   );
 }
 
